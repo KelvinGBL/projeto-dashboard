6a5565b19a68fce8b4fe8867311bed20f3f73a91
Kelvin Gabriel <109625159+KelvinGBL@users.noreply.github.com>
2024-11-10 19:34:09 -0300

    Create Components

---
+++ components/sidebar.php
@@ -0,0 +1,21 @@
+        <nav class="sidebar close">
+            <header>
+                <i class='logo-icon bx bxl-chrome'></i>
+                <span class="logo-name">Logo</span>
+            </header>
+
+            <ul class="nav-links">
+                <li>
+                    <a href="index.php">
+                        <i class='bx bx-grid-alt'></i>
+                        <span class="link-name">Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="usuarios.php">
+                        <i class='bx bxs-group'></i>
+                        <span class="link-name">Usuários</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
